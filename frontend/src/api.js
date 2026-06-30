671c361246749c7ee19d36f4ed80e4f22fef2a57
eugeniapcastro28 <eugeniapcastro28@gmail.com>
2026-06-30 19:05:42 +0800

    change url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:4000/api';
+const API_BASE = 'https://expensetracker-w139.onrender.com/api';
 
 export async function signup(email, password) {
   const res = await fetch(`${API_BASE}/auth/signup`, {
